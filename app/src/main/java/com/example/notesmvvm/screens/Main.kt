4de56d9c0f887efe9e3f6bed631e09e9a4fdb307
AmazingHorsess <amazinghorsess@gmail.com>
2023-03-29 19:26:45 +0600

    Model VM DB implementation

--- app/src/main/java/com/example/notesmvvm/screens/Main.kt
+++ app/src/main/java/com/example/notesmvvm/screens/Main.kt
@@ -1,10 +1,13 @@
 package com.example.notesmvvm.screens
 
 import android.annotation.SuppressLint
+import android.app.Application
 import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -19,13 +22,22 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.example.notesmvvm.navigation.NavRoute
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.notesmvvm.MainViewModel
+import com.example.notesmvvm.MainViewModelFactory
+import com.example.notesmvvm.model.NoteModel
 import com.example.notesmvvm.ui.theme.NotesMVVMTheme
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun Main(navController: NavHostController){
+    val context = LocalContext.current
+    val mViewModel: MainViewModel = viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+    val notes = mViewModel.readTest.observeAsState(listOf()).value
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -40,17 +52,25 @@ fun Main(navController: NavHostController){
             }
         }
     ) {
-        Column() {
-            NoteItem(title = "Note 1", subtitle = "Subtitle for note 1", navController = navController)
-            NoteItem(title = "Note 2", subtitle = "Subtitle for note 2", navController = navController)
-            NoteItem(title = "Note 3", subtitle = "Subtitle for note 3", navController = navController)
-            NoteItem(title = "Note 4", subtitle = "Subtitle for note 4", navController = navController)
+        //Column() {
+        //    NoteItem(title = "Note 1", subtitle = "Subtitle for note 1", navController = navController)
+        //    NoteItem(title = "Note 2", subtitle = "Subtitle for note 2", navController = navController)
+        //    NoteItem(title = "Note 3", subtitle = "Subtitle for note 3", navController = navController)
+        //    NoteItem(title = "Note 4", subtitle = "Subtitle for note 4", navController = navController)
+        //}
+        LazyColumn {
+            items(notes) {
+                notes ->
+                NoteItem(note = notes, navController = navController )
+            }
+
         }
+
     }
 }
 
 @Composable
-fun NoteItem(title: String, subtitle: String, navController: NavHostController) {
+fun NoteItem(note: NoteModel, navController: NavHostController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -65,11 +85,11 @@ fun NoteItem(title: String, subtitle: String, navController: NavHostController) 
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = title,
+                text = note.title,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
-            Text(text = subtitle)
+            Text(text = note.subtitle)
         }
     }
 }
--- app/src/main/java/com/example/notesmvvm/screens/Start.kt
+++ app/src/main/java/com/example/notesmvvm/screens/Start.kt
@@ -1,6 +1,7 @@
 package com.example.notesmvvm.screens
 
 import android.annotation.SuppressLint
+import android.app.Application
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -8,16 +9,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.notesmvvm.MainViewModel
+import com.example.notesmvvm.MainViewModelFactory
 import com.example.notesmvvm.navigation.NavRoute
 import com.example.notesmvvm.ui.theme.NotesMVVMTheme
+import com.example.notesmvvm.utils.TYPE_FIREBASE
+import com.example.notesmvvm.utils.TYPE_ROOM
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun StartScreen(navController: NavHostController){
+    val context = LocalContext.current
+    val mViewModel: MainViewModel = viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
     Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -27,6 +36,7 @@ fun StartScreen(navController: NavHostController){
             Text(text = "Что следует использовать?")
             Button(
                 onClick = {
+                    mViewModel.initDatabase(TYPE_ROOM)
                     navController.navigate(route = NavRoute.Main.route) },
             modifier = Modifier
                 .width(200.dp)
@@ -35,6 +45,7 @@ fun StartScreen(navController: NavHostController){
             }
             Button(
                 onClick = {
+                    mViewModel.initDatabase(TYPE_FIREBASE)
                     navController.navigate(route = NavRoute.Main.route)
                 },
                 modifier = Modifier
@@ -43,13 +54,8 @@ fun StartScreen(navController: NavHostController){
             ) {
                 Text(text = "Firebase database")
             }
-
         }
-
     }
-
-
-
 }
 
 @Preview(showBackground = true)
